a4dd31c14bbf2ea8c874f484fd826ab9609ed2f3
Benjamin Pradon <pradonbenjamin@gmail.com>
2020-12-21 17:04:08 +0100

    did the merge after a few setting and put everything into json format to use it for data Viz

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ import pandas as pd
 data = pd.read_json(r'paris_lines_systems_and_modes.json')
 data = data[data.system_name == "Métro de Paris"]
 data = data[data.color != "#000"]
+data.columns = ['id','Ligne Horizon','url_name','color','system_id','systeme_name','transport_mode_id','transport_mode_name']
+dataDrop = data.drop(20)
 
 # parse wiki table
 data_wiki = pd.read_html('https://fr.wikipedia.org/wiki/M%C3%A9tro_de_Paris')
@@ -34,14 +36,14 @@ dataFrame_wiki.loc[15,'Ligne Horizon']='M 14'
 # looking for a way to delete duplicate
 Finaldata = dataFrame_wiki.drop(16)
 
-print(Finaldata)
-
 #joining the data like in SQL for use in Web
 # https://pandas.pydata.org/pandas-docs/stable/user_guide/merging.html#database-style-dataframe-or-named-series-joining-merging
-
+DataForViz = pd.merge(dataDrop, Finaldata, how='inner', on='Ligne Horizon')
+print(DataForViz)
 
 #putting the new dataset (from join in json format)
-
+DataForViz = DataForViz.to_json()
+print(DataForViz)
 
 # parse wiki table 
 dem_paris = pd.read_html('https://fr.wikipedia.org/wiki/Mod%C3%A8le:Tableau_D%C3%A9mographie_Paris#Carte')
